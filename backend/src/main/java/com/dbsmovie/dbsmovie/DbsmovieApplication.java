0a88b73fc9b8d7e6a4db970be47037573411090c
Dâmares Bortolucci de Sousa <damares_sp78@yahoo.com.br>
2022-01-13 16:50:33 -0300

    Project created

---
+++ backend/src/main/java/com/dbsmovie/dbsmovie/DbsmovieApplication.java
@@ -0,0 +1,13 @@
+package com.dbsmovie.dbsmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbsmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbsmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dbsmovie/dbsmovie/DbsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbsmovie.dbsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
