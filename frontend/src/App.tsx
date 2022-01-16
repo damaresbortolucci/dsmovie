82b357fae6a82dd7d5e96027c6b6462438d8e0aa
Dâmares Bortolucci de Sousa <damares_sp78@yahoo.com.br>
2022-01-16 19:50:11 -0300

    add navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,12 +5,13 @@ import {
 } from "react-router-dom";
 import Listing from '../src/pages/Listing';
 import Form from '../src/pages/Form';
-//import Navbar from "../src/components/Navbar";
+import Navbar from "components/Navbar";
+
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <Navbar /> */}
+      <Navbar />
       <Routes>
         <Route path="/" element={<Listing />} />
         <Route path="/form">
