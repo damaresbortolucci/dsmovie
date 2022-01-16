8d952d84c7a3d86d99c53a0e44060094cebcd23c
Dâmares Bortolucci de Sousa <damares_sp78@yahoo.com.br>
2022-01-16 19:28:11 -0300

    correcao form

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -35,9 +35,7 @@ function Form() {
                     <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
                 </div>
             </form >
-            <Link to="/">
-                <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
-            </Link> 
+            <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
         </div >
     </div >
     )
