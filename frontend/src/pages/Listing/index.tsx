4ea9bce2c244453daff8150a9e0b50146c8fed64
Dâmares Bortolucci de Sousa <damares_sp78@yahoo.com.br>
2022-01-16 21:14:44 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,14 @@
+import axios from 'axios';
+import { BASE_URL } from 'utils/requests';
 import MovieCard from '../../components/MovieCard';
 import Pagination from '../../components/Pagination';
 
 function Listing() {
+
+    axios.get(`${BASE_URL}/movies?size=12`).then(response=> {
+        console.log(response.data)
+    })
+
     return (
         <>
             <Pagination />
